c9faeaaa7c5dc8480eca28d9d261bbc988ceb01c
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-11-15 16:56:12 +0900

    lint

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ registerBlockType( 'my-first-block/hello', {
 	title: 'hello',
 	icon: 'palmtree',
 	category: 'common',
-	edit({ className } ) {
+	edit( { className } ) {
 		return ( <div className={ className }>エディターだよ。</div> );
 	},
-	save({ className } ) {
+	save( { className } ) {
 		return ( <div className={ className }>保存されるデータ、実際に表示されるテキスト。</div> );
 	},
 } );
